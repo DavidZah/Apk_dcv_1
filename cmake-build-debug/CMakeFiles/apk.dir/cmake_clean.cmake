2b5e49fc2d4c2fe5644c3e5641818a461a9901ad
David Žahour <davidzah6@seznam.cz>
2020-09-25 22:28:14 +0200

    build

---
+++ cmake-build-debug/CMakeFiles/apk.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/apk.dir/main.c.obj"
+  "apk.exe"
+  "apk.exe.manifest"
+  "apk.pdb"
+  "libapk.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/apk.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
